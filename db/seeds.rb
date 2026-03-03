678f347624f672f8140708a3c7b4924eef09f5e7
Enrique Verea <kikeverea@gmail.com>
2026-03-04 00:05:56 +0100

    Merge from remote

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 #   end
 
 User.find_or_create_by!(email: "admin@admin.com", role: "admin") do |user|
-  user.password = "12345678"
+  user.password = "12341234"
   user.name = "Admin"
   user.lastname = "Innobing"
 end
