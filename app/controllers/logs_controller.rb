a73f9afd8f5a5deb0da54d926f0c97aea0bb6724
Enrique Verea <kikeverea@gmail.com>
2026-03-05 22:02:19 +0100

    Improve initializers and defaults

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -29,8 +29,8 @@ class LogsController < ApplicationController
         format.html { redirect_to log_url(@log), notice: "Log was successfully created." }
         format.json { render :show, status: :created, location: @log }
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @log.errors, status: :unprocessable_entity }
+        format.html { render :new, status: :unprocessable_content }
+        format.json { render json: @log.errors, status: :unprocessable_content }
       end
     end
   end
@@ -42,8 +42,8 @@ class LogsController < ApplicationController
         format.html { redirect_to log_url(@log), notice: "Log was successfully updated." }
         format.json { render :show, status: :ok, location: @log }
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @log.errors, status: :unprocessable_entity }
+        format.html { render :edit, status: :unprocessable_content }
+        format.json { render json: @log.errors, status: :unprocessable_content }
       end
     end
   end
