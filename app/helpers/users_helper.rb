7360e687bfd984d606ba1df7ffc618f4e52b9613
Enrique Verea <kikeverea@gmail.com>
2026-03-06 01:18:04 +0100

    Define user roles

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+  def user_role_text(role)
+    I18n.t("activerecord.enums.user.role.#{role}")
+  end
+
+  def user_status_text(status)
+    I18n.t("activerecord.enums.user.status.#{status}")
+  end
+
 end
