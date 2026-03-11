c321582643b31fd7543b7570897fdec6c0ea0450
Innobing <innobingpruebas@gmail.com>
2026-03-11 10:19:17 +0100

    Fix root url for admins

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,5 +4,9 @@ class AdminController < ApplicationController
   def index
     @title = "Dashboard"
     @logs = Log.all.order(id: :desc)
+
+    redirect_to users_path if current_user.super_admin? || current_user.company_admin?
+    redirect_to web_companies_path if current_user.user?
+
   end
 end
